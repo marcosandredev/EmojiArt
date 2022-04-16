5218a6f0b9d99a94569548327a9717e1f7600bd4
Marcos Lara <marcos.andre.dev@outlook.com>
2022-04-16 20:54:16 -0300

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Marcos André Novaes de Lara on 16/04/22.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
