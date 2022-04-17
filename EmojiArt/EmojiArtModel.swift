1b672727b316629a5edfa668c91c33406bd4d823
Marcos Lara <marcos.andre.dev@outlook.com>
2022-04-17 20:18:04 -0300

    vs 0.003 - Aula 11 - Error Handling Persistence, Decoder and Encoder

--- EmojiArt/EmojiArtModel.swift
+++ EmojiArt/EmojiArtModel.swift
@@ -8,11 +8,11 @@
 
 import Foundation
 
-struct EmojiArtModel {
+struct EmojiArtModel: Codable {
   var background = Background.blank
   var emojis = [Emoji]()
   
-  struct Emoji: Identifiable, Hashable {
+  struct Emoji: Identifiable, Hashable, Codable {
     let text: String
     var x: Int // deslocados do centro
     var y: Int // deslocados do centro
@@ -26,6 +26,19 @@ struct EmojiArtModel {
       self.size = size
       self.id = id
     }
+  }
+  
+  func json() throws -> Data {
+    return try JSONEncoder().encode(self)
+  }
+  
+  init(json: Data) throws {
+    self = try JSONDecoder().decode(EmojiArtModel.self, from: json)
+  }
+  
+  init(url: URL) throws {
+    let data = try Data(contentsOf: url)
+    self = try EmojiArtModel(json: data)
   }
   
   init() {}
